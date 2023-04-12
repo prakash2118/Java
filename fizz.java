a9d05782a74b09493dbd76afc98ea00dcd5cd2bb
prakash2118 <90789267+prakash2118@users.noreply.github.com>
2023-04-12 20:51:21 +0530

    Create fizz.java

---
+++ fizz.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter a Number: ");
+        int number= scanner.nextInt();
+        {
+            if (number % 5 == 0)
+                System.out.println("Fizz");
+            else if (number % 3 == 0)
+                System.out.println("Buzz");
+            else if (number % 5 == 0 || number % 3 == 0)
+                System.out.println("fizz buzz");
+            else
+                System.out.println(number);
+        }
+    }
+}
